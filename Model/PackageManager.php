78e2cfc6b37c101cfea77eea6755e701a7f19581
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2019-11-01 10:58:52 +0200

    Module update action. This will be rewritten to use cron jobs to fix memory and permission issues.

--- Model/PackageManager.php
+++ Model/PackageManager.php
@@ -8,17 +8,35 @@ class PackageManager
         \Magento\Framework\Module\PackageInfo $packageInfo,
         \Magento\Framework\Module\Status $moduleStatus,
         \Magento\Framework\Code\GeneratedFiles $generatedFiles,
+        \Magento\Framework\Composer\MagentoComposerApplicationFactory $composerApplicationFactory,
         \Swissup\Marketplace\Model\ResourceModel\Package\CollectionFactory $packageCollectionFactory
     ) {
         $this->packageInfo = $packageInfo;
         $this->moduleStatus = $moduleStatus;
         $this->generatedFiles = $generatedFiles;
         $this->packageCollection = $packageCollectionFactory->create();
+        $this->composerApplicationFactory = $composerApplicationFactory;
     }
 
     public function get($packageName)
     {
         return $this->packageCollection->getItemById($packageName);
+    }
+
+    public function update($packageName)
+    {
+        ini_set('memory_limit', '1G');
+
+        return $this->composerApplicationFactory
+            ->create()
+            ->runComposerCommand([
+                'command' => 'update',
+                'packages' => [$packageName],
+                '--with-all-dependencies' => true,
+                '--no-interaction' => true,
+                '--no-dev' => true,
+                '--no-progress' => true,
+            ]);
     }
 
     public function disable($packageName)
