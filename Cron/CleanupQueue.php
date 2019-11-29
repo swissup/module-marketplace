f4149ba7a09b23fc22556a06be468a7f2c8defe9
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2019-11-29 14:55:51 +0200

    Queue cleanup added.

---
+++ Cron/CleanupQueue.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Swissup\Marketplace\Cron;
+
+use Magento\Framework\Stdlib\DateTime;
+
+class CleanupQueue
+{
+    /**
+     * @var \Swissup\Marketplace\Model\ResourceModel\Job\CollectionFactory
+     */
+    private $collectionFactory;
+
+    /**
+     * @param \Swissup\Marketplace\Model\ResourceModel\Job\CollectionFactory $collectionFactory
+     */
+    public function __construct(
+        \Swissup\Marketplace\Model\ResourceModel\Job\CollectionFactory $collectionFactory
+    ) {
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    /**
+     * Removes records older that 1 week.
+     * Skips 50 records.
+     */
+    public function execute()
+    {
+        $date = (new \DateTime('-1 week'))->format(DateTime::DATETIME_PHP_FORMAT);
+        $jobs = $this->collectionFactory->create()
+            ->addFieldToFilter('scheduled_at', [
+                'or' => [
+                    ['date' => true, 'to' => $date],
+                    ['is' => new \Zend_Db_Expr('null')],
+                ]
+            ])
+            ->addFieldToFilter('created_at', [
+                ['date' => true, 'to' => $date]
+            ])
+            ->setOrder('created_at', 'ASC')
+            ->setPageSize(250);
+
+        $itemsToRemove = $jobs->count() - 50;
+
+        foreach ($jobs as $job) {
+            if ($itemsToRemove-- < 0) {
+                break;
+            }
+
+            $job->delete();
+        }
+    }
+}
