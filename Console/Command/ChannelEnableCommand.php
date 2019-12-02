28074eb868f8691bd3b10678d668525abe19b2d2
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2019-12-02 10:00:17 +0200

    Channel:enable command improved - aks for credentials if needed.

--- Console/Command/ChannelEnableCommand.php
+++ Console/Command/ChannelEnableCommand.php
@@ -3,9 +3,12 @@
 namespace Swissup\Marketplace\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\QuestionFactory;
+use Swissup\Marketplace\Api\ChannelInterface;
 
 class ChannelEnableCommand extends Command
 {
@@ -22,15 +25,29 @@ class ChannelEnableCommand extends Command
     private $channelManager;
 
     /**
+     * @var InputInterface
+     */
+    private $input;
+
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+
+    /**
      * @param \Swissup\Marketplace\Model\ChannelRepository $channelRepository
      * @param \Swissup\Marketplace\Model\ChannelManager $channelManager
      */
     public function __construct(
         \Swissup\Marketplace\Model\ChannelRepository $channelRepository,
-        \Swissup\Marketplace\Model\ChannelManager $channelManager
+        \Swissup\Marketplace\Model\ChannelManager $channelManager,
+        QuestionFactory $questionFactory,
+        QuestionHelper $questionHelper
     ) {
         $this->channelRepository = $channelRepository;
         $this->channelManager = $channelManager;
+        $this->questionFactory = $questionFactory;
+        $this->questionHelper = $questionHelper;
         parent::__construct();
     }
 
@@ -56,6 +73,9 @@ class ChannelEnableCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->input = $input;
+        $this->output = $output;
+
         try {
             $enabled = [];
             $channels = $input->getArgument(self::INPUT_KEY_CHANNELS);
@@ -67,6 +87,8 @@ class ChannelEnableCommand extends Command
 
                 $this->channelManager->enable($channel);
                 $enabled[] = $channel->getIdentifier();
+
+                $this->saveCredentials($channel);
             }
 
             if (!$enabled) {
@@ -88,5 +110,60 @@ class ChannelEnableCommand extends Command
 
             return \Magento\Framework\Console\Cli::RETURN_FAILURE;
         }
+    }
+
+    /**
+     * @param ChannelInterface $channel
+     * @return void
+     */
+    protected function saveCredentials(ChannelInterface $channel)
+    {
+        if (!$channel->getAuthType()) {
+            return;
+        }
+
+        $username = $channel->getUsername();
+        $password = $channel->getPassword();
+
+        if ($username && $password) {
+            return;
+        }
+
+        if (!$username) {
+            $channel->addData([
+                'username' => $this->ask('Please enter your username: '),
+            ]);
+        }
+
+        if (!$password) {
+            $channel->addData([
+                'password' => $this->ask('Please enter your password/private_key/access_key: '),
+            ]);
+        }
+
+        $packages = $channel->getPackages();
+
+        $this->output->writeln('<info>Credentials has been accepted</info>');
+        $this->output->writeln('<info>Channel returned ' . count($packages) . ' package(s)</info>');
+
+        $this->channelManager->saveCredentials($channel);
+    }
+
+    /**
+     * @param string $question
+     * @return string
+     */
+    protected function ask($question)
+    {
+        $question = $this->questionFactory->create(['question' => $question]);
+        $question->setValidator(function ($value) {
+            if (trim($value) == '') {
+                throw new \Exception('Value cannot be empty');
+            }
+
+            return $value;
+        });
+
+        return $this->questionHelper->ask($this->input, $this->output, $question);
     }
 }
