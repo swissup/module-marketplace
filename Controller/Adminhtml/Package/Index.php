8e65adf8b07780f2e07524b2081c17a56b97f539
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2019-12-27 17:31:30 +0200

    Check if cron is working

--- Controller/Adminhtml/Package/Index.php
+++ Controller/Adminhtml/Package/Index.php
@@ -3,6 +3,7 @@
 namespace Swissup\Marketplace\Controller\Adminhtml\Package;
 
 use Magento\Backend\App\Action\Context;
+use Magento\Framework\Stdlib\DateTime;
 use Magento\Framework\View\Result\PageFactory;
 
 class Index extends \Magento\Backend\App\Action
@@ -17,13 +18,16 @@ class Index extends \Magento\Backend\App\Action
     /**
      * @param Context $context
      * @param PageFactory $resultPageFactory
+     * @param \Magento\Cron\Model\ResourceModel\Schedule\CollectionFactory $cronCollectionFactory
      */
     public function __construct(
         Context $context,
-        PageFactory $resultPageFactory
+        PageFactory $resultPageFactory,
+        \Magento\Cron\Model\ResourceModel\Schedule\CollectionFactory $cronCollectionFactory
     ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
+        $this->cronCollectionFactory = $cronCollectionFactory;
     }
 
     /**
@@ -33,11 +37,31 @@ class Index extends \Magento\Backend\App\Action
      */
     public function execute()
     {
+        $this->validateCron();
+
         /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
         $resultPage = $this->resultPageFactory->create();
         $resultPage->setActiveMenu('Swissup_Marketplace::package_index');
         $resultPage->addBreadcrumb(__('Marketplace'), __('Marketplace'));
         $resultPage->getConfig()->getTitle()->prepend(__('Package Manager'));
         return $resultPage;
+    }
+
+    protected function validateCron()
+    {
+        $jobs = $this->cronCollectionFactory->create()
+            ->addFieldToFilter('job_code', 'swissup_marketplace_job_run')
+            ->addFieldToFilter(
+                'created_at',
+                [
+                    'gt' => (new \DateTime('-1 hour'))->format(DateTime::DATETIME_PHP_FORMAT)
+                ]
+            );
+
+        if (!$jobs->count()) {
+            $this->messageManager->addError(__(
+                "Cron is not configured properly. Marketplace won't work without cron."
+            ));
+        }
     }
 }
