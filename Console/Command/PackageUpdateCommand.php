c0be8511c515f7c657ed0ecc35f5d58f6a59525e
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2020-01-29 16:01:02 +0200

    Update package command added

---
+++ Console/Command/PackageUpdateCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Swissup\Marketplace\Console\Command;
+
+use Swissup\Marketplace\Model\Handler\PackageUpdate;
+
+class PackageUpdateCommand extends PackageAbstractCommand
+{
+    /**
+     * @var string
+     */
+    protected $handlerClass = PackageUpdate::class;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('marketplace:package:update')
+            ->setDescription('Update specified packages');
+
+        parent::configure();
+    }
+}
