6ec2c8a4c5dd823a61551e3c85867348c37358ef
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2020-02-10 16:01:20 +0200

    CopyMediaDir command

---
+++ Installer/Command/CopyMediaDir.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Swissup\Marketplace\Installer\Command;
+
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Swissup\Marketplace\Installer\Request;
+use Swissup\Marketplace\Model\Traits\LoggerAware;
+
+class CopyMediaDir
+{
+    use LoggerAware;
+
+    public function __construct(\Magento\Framework\Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * @param Request $request
+     * @return void
+     */
+    public function execute(Request $request)
+    {
+        $this->logger->info('RESOURCES: Copy media files');
+
+        $media = $this->filesystem->getDirectoryWrite(DirectoryList::MEDIA);
+        $driver = $media->getDriver();
+        $mediaPath = $media->getAbsolutePath();
+
+        foreach ($request->getParams() as $dir) {
+            $paths = $driver->readDirectoryRecursively($dir);
+            $paths = array_reverse($paths); // put deepest in the end
+
+            foreach ($paths as $path) {
+                $relative = str_replace($dir . '/', '', $path);
+                $destination = $mediaPath . $relative;
+
+                try {
+                    if ($driver->isExists($destination)) {
+                        continue;
+                    }
+
+                    if ($driver->isDirectory($path)) {
+                        $driver->createDirectory($destination);
+                    } else {
+                        $driver->copy($path, $destination);
+                    }
+                } catch (\Exception $e) {
+                    $this->logger->warning($e->getMessage());
+                }
+            }
+        }
+    }
+}
