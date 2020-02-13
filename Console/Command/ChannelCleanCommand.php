cadb5cea49a98fce80b30c218908b0df70c75575
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2020-02-13 14:52:24 +0200

    Cache clean command added

---
+++ Console/Command/ChannelCleanCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Swissup\Marketplace\Console\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ChannelCleanCommand extends ChannelAbstractCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('marketplace:channel:clean')
+            ->setDescription('Clean cache for specified channel');
+
+        parent::configure();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $this->getChannel()->removeCache();
+
+            $output->writeln('<info>The cache was cleaned</info>');
+
+            return \Magento\Framework\Console\Cli::RETURN_SUCCESS;
+        } catch (\Exception $e) {
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+                $output->writeln($e->getTraceAsString());
+            }
+
+            return \Magento\Framework\Console\Cli::RETURN_FAILURE;
+        }
+    }
+}
