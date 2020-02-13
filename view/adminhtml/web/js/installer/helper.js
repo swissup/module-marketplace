e337993afeee7d62dce0e5efafcb03a877e3f0d2
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2020-02-13 10:29:37 +0200

    Don't show installer when using massaction if it's not available

--- view/adminhtml/web/js/installer/helper.js
+++ view/adminhtml/web/js/installer/helper.js
@@ -1,6 +1,7 @@
 define([
+    'underscore',
     'uiRegistry'
-], function (registry) {
+], function (_, registry) {
     'use strict';
 
     return {
@@ -13,7 +14,26 @@ define([
                     'swissup_marketplace_package_listing',
                     'marketplace_installer'
                 ].join('.'),
-                form = registry.get(modal + '.installer_form');
+                form = registry.get(modal + '.installer_form'),
+                source = [
+                    'swissup_marketplace_package_listing',
+                    'swissup_marketplace_package_listing_data_source'
+                ].join('.'),
+                hasInstaller;
+
+            source = registry.get(source);
+
+            hasInstaller = _.every(packages, function (packageName) {
+                var packageData = _.find(source.rows, function (row) {
+                    return row.name === packageName;
+                });
+
+                return packageData && packageData.installer;
+            }, this);
+
+            if (!hasInstaller) {
+                return;
+            }
 
             form.params.packages = packages;
 
