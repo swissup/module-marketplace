4b65306e2832743b4e5370f32af2dd4d08bf8ee3
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2020-02-21 17:23:24 +0200

    Added ability to select all visible packages with one click

---
+++ view/adminhtml/web/js/packages/grid/columns/multiselect.js
@@ -0,0 +1,85 @@
+define([
+    'Magento_Ui/js/grid/columns/multiselect',
+    'mage/translate'
+], function (Multiselect, $t) {
+    'use strict';
+
+    return Multiselect.extend({
+        defaults: {
+        },
+
+        /**
+         * Initializes column component.
+         *
+         * @returns {Column} Chainable.
+         */
+        initialize: function () {
+            this._super();
+
+            this.actions = [{
+                value: 'selectAll',
+                label: $t('Select All on This Page')
+            }, {
+                value: 'deselectAll',
+                label: $t('Deselect All')
+            }, {
+                value: 'deselectPage',
+                label: $t('Deselect All on This Page')
+            }];
+
+            return this;
+        },
+
+        /**
+         * Initializes observable properties.
+         *
+         * @returns {Multiselect} Chainable.
+         */
+        initObservable: function () {
+            this._super();
+
+            /**
+             * Disable exclude mode
+             * @return {Boolean}
+             */
+            this.excludeMode = function () {
+                return false;
+            };
+
+            /**
+             * Disable allSelected mode
+             * @return {Boolean}
+             */
+            this.allSelected = function () {
+                return this.isPageSelected(true);
+            };
+
+            return this;
+        },
+
+        /**
+         * @returns {Multiselect} Chainable.
+         */
+        selectAll: function () {
+            return this.selectPage();
+        },
+
+        /**
+         * Checks if current page has selected records.
+         *
+         * @param {Boolean} [all=false] - If set to 'true' checks that every
+         *      record on the page is selected. Otherwise checks that
+         *      page has some selected records.
+         * @returns {Boolean}
+         */
+        isPageSelected: function (all) {
+            var ids = this.getIds(),
+                selected = this.selected(),
+                iterator = all ? 'every' : 'some';
+
+            return ids[iterator](function (id) {
+                return !!~selected.indexOf(id);
+            });
+        }
+    });
+});
