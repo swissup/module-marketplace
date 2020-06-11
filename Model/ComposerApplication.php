e95effeecca9ce87bcd046b1db13e61b6e15b284
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2020-06-11 18:30:20 +0300

    Fixed compatibility with composer < 1.6.0

--- Model/ComposerApplication.php
+++ Model/ComposerApplication.php
@@ -50,10 +50,7 @@ class ComposerApplication
      */
     public function run(array $command)
     {
-        // https://github.com/composer/composer/commit/94df55425596c0137d0aa14485bba2fb05e15de6
-        if (version_compare($this->app->getVersion(), '1.8.4', '<')) {
-            unset($command['-q']);
-        }
+        $command = $this->normalizeCommand($command);
 
         $this->app->resetComposer();
 
@@ -83,5 +80,34 @@ class ComposerApplication
             '-a' => true,
             '-g' => true,
         ], $command));
+    }
+
+    /**
+     * @param array $command
+     * @return array
+     */
+    protected function normalizeCommand(array $command)
+    {
+        // https://github.com/composer/composer/commit/94df55425596c0137d0aa14485bba2fb05e15de6
+        if (version_compare($this->app->getVersion(), '1.8.4', '<')) {
+            unset($command['-q']);
+        }
+
+        // https://github.com/composer/composer/commit/0e192ced6943a7a6e3de7c52ec0287d115d05784
+        if (version_compare($this->app->getVersion(), '1.6.0', '<')) {
+            $replace = [
+                '--update-with-all-dependencies' => '--update-with-dependencies',
+                '--with-all-dependencies' => '--with-dependencies',
+            ];
+
+            foreach ($replace as $search => $replace) {
+                if (isset($command[$search])) {
+                    $command[$replace] = $command[$search];
+                    unset($command[$search]);
+                }
+            }
+        }
+
+        return $command;
     }
 }
