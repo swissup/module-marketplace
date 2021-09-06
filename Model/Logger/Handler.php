9586d58cd1ab633695abd3aa2f4793a38e63ad34
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-09-06 09:36:08 +0300

    Fixed compatibility with monolog 2.0. #15

--- Model/Logger/Handler.php
+++ Model/Logger/Handler.php
@@ -4,13 +4,14 @@ namespace Swissup\Marketplace\Model\Logger;
 
 use Monolog\Formatter\FormatterInterface;
 use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\HandlerInterface;
 
 class Handler extends \Magento\Framework\Logger\Handler\Base
 {
     /**
      * {@inheritdoc}
      */
-    public function setFormatter(FormatterInterface $formatter)
+    public function setFormatter(FormatterInterface $formatter): HandlerInterface
     {
         $this->formatter = new LineFormatter("%message%\n", null, true);
 
